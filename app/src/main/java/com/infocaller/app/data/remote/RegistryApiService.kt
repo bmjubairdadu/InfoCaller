3ed3e7dcb5a1cd7c7853d81736d95f3bcff11734
Jubair Hosen <jubairhossen441@gmail.com>
2026-09-03 18:26:43 +0600

    feat: expose shard registry API

--- app/src/main/java/com/infocaller/app/data/remote/RegistryApiService.kt
+++ app/src/main/java/com/infocaller/app/data/remote/RegistryApiService.kt
@@ -2,6 +2,8 @@ package com.infocaller.app.data.remote
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface RegistryApiService {
@@ -9,7 +11,8 @@ interface RegistryApiService {
     suspend fun fetchManifest(@Url url: String): Response<com.google.gson.JsonObject>
 
     @GET("api/v1/registry/lookup/{number}")
-    suspend fun lookupInRegistry(
-        @retrofit2.http.Path("number") number: String
-    ): Response<com.infocaller.app.data.remote.model.InfoCallerLookupResponse>
+    suspend fun lookupInRegistry(@Path("number") number: String): Response<com.google.gson.JsonObject>
+
+    @GET("api/v1/registry/shard")
+    suspend fun fetchShard(@Query("path") path: String): Response<RegistryShardResponse>
 }
